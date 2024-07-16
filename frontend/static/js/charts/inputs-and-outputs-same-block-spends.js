faefbf87c0c02316e4bf7e0ceb947dc8ea0998f1
0xb10c <b10c@b10c.me>
2024-07-16 10:15:29 +0200

    update: Inputs spending UTXOs created in the same Block
    Was trying to load the csv files from the old backend.

--- frontend/static/js/charts/inputs-and-outputs-same-block-spends.js
+++ frontend/static/js/charts/inputs-and-outputs-same-block-spends.js
@@ -2,7 +2,7 @@ const chartRollingAverage = 7
 
 const CSVs = [
   d3.csv("/csv/date.csv"),
-  d3.csv("/csv/UTXOSpendInCreationBlock_sum.csv"),
+  d3.csv("/csv/inputs_spend_in_same_block_sum.csv"),
   d3.csv("/csv/inputs_sum.csv"),
 ]
 
@@ -10,7 +10,7 @@ function preprocess(data) {
   combinedData = []
   for (let i = 0; i < data[0].length; i++) {
     const date = d3.timeParse("%Y-%m-%d")(data[0][i].date)
-    const y =  parseFloat(data[1][i].UTXOSpendInCreationBlock_sum) / parseFloat(data[2][i].inputs_sum)
+    const y =  parseFloat(data[1][i].inputs_spend_in_same_block_sum) / parseFloat(data[2][i].inputs_sum)
     combinedData.push({date, y})
   }
   return combinedData
@@ -20,7 +20,7 @@ const annotations = []
 const labels = {"y": "spending newly created UTXOs"}
 const dataType = dataTypePercentage
 const unit = ""
-const chartFunction = lineWithAreaChart  
+const chartFunction = lineWithAreaChart
 
 var yDomain = (_ => [0, 1])
 yAxis.tickFormat(d3.format("~p"));
