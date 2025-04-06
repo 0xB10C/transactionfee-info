f724c5b6e43e33857a536839f049e7faf2792d21
0xb10c <b10c@b10c.me>
2025-04-06 19:14:53 +0200

    add: cumulative (log2) work and PoW-equivalent days

---
+++ frontend/content/charts/mining-cumulative-work.md
@@ -0,0 +1,20 @@
+---
+title: "Cumulative Work"
+draft: false
+author: "0xb10c"
+categories: Mining
+categories_weight: 0
+tags: ["cumulative", "mining", "work"]
+thumbnail: mining-cumulative-work.png
+chartJS: mining-cumulative-work.js
+images:
+  - /img/chart-thumbnails/mining-cumulative-work.png
+---
+
+Shows the cumulative work done.
+
+<!--more-->
+
+“Work” is the expected number of block header hash attempts required to mine a block.
+
+Note that there might be slight inaccuracies due to JavaScript's handling of large integers.
---
+++ frontend/static/js/charts/mining-cumulative-log2work.js
@@ -0,0 +1,33 @@
+const chartRollingAverage = 1
+
+const CSVs = [
+  d3.csv("/csv/date.csv"),
+  d3.csv("/csv/log2_work_avg.csv"),
+  d3.csv("/csv/inputs_coinbase_sum.csv"),
+  d3.csv("/csv/inputs_witness_coinbase_sum.csv"),
+]
+
+function preprocess(data) {
+  let combinedData = []
+  let cumulative_work = 0n
+  for (let i = 0; i < data[0].length; i++) {
+    const date = d3.timeParse("%Y-%m-%d")(data[0][i].date)
+    const log2_work = parseFloat(data[1][i].log2_work_avg)
+    const blocks_per_day = (parseFloat(data[2][i].inputs_coinbase_sum) + parseFloat(data[3][i].inputs_witness_coinbase_sum))
+    cumulative_work += BigInt(Math.round(2 ** log2_work) * blocks_per_day)
+    const y = Math.log2(Number(cumulative_work));
+    combinedData.push({date, y})
+  }
+
+  return combinedData
+}
+
+const annotations = [annotationChinaMiningBan]
+const labels = {"y": "work"}
+const dataType = dataTypeMetric
+const unit = ""
+
+var yValue = (d => d.y);
+var yDomain = (data => [0, d3.max(data, d => (xScale.domain()[0] <= d.date && xScale.domain()[1] > d.date) ? yValue(d) : 0)])
+const chartFunction = lineWithAreaChart
+const startDate = d3.timeParse("%Y-%m-%d")("2009-01-03")
