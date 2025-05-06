0f5f07da9fb67ff5725b9e39a5fb28fccd9de83e
0xb10c <b10c@b10c.me>
2025-05-06 17:14:37 +0200

    fix: unenforced locktime detection
    closes #54

--- frontend/static/js/charts/transactions-not-enforced-locktime.js
+++ frontend/static/js/charts/transactions-not-enforced-locktime.js
@@ -1,27 +1,22 @@
 const ANNOTATIONS = []
 const MOVING_AVERAGE_DAYS = MOVING_AVERAGE_7D
-const NAME = "lock-by-block-height"
+const NAME = "unenforced locktime"
 const PRECISION = 2
-let START_DATE =  new Date("2015");
+let START_DATE = new Date("2014");
 
 
 const CSVs = [
   fetchCSV("/csv/date.csv"),
   fetchCSV("/csv/tx_timelock_not_enforced_sum.csv"),
   fetchCSV("/csv/transactions_sum.csv"),
-  fetchCSV("/csv/tx_timelock_height_sum.csv"),
-  fetchCSV("/csv/tx_timelock_timestamp_sum.csv"),
 ]
 
-// TODO: this calculation is incorrect:
-// https://github.com/0xB10C/transactionfee-info/issues/54
 function preprocess(input) {
   let data = { date: [], y: [] }
   for (let i = 0; i < input[0].length; i++) {
     data.date.push(+(new Date(input[0][i].date)))
     const not_enforced = parseFloat(input[1][i].tx_timelock_not_enforced_sum)
     const all_tx = parseFloat(input[2][i].transactions_sum)
-    const timelocked = parseFloat(input[3][i].tx_timelock_height_sum) + parseFloat(input[4][i].tx_timelock_timestamp_sum)
     const y = (not_enforced/all_tx)
     data.y.push(y * 100)
   }
